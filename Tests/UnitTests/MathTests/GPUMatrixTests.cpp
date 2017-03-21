8af12587a998a027ff86402b276c509036cee30f
Alexey Reznichenko <alrezni@microsoft.com>
2017-03-21 17:27:04 +0100

    Disable CUDA P2P on Linux
      CUDA p2p (uva) leads to silent hangs on linux with IOMMU DMAR enabled
      and since there's no way to check for IOMMU conditions at the moment
      (NVIDIA is supposedly looking into this), disable the p2p mem copy
      for now.

--- Tests/UnitTests/MathTests/GPUMatrixTests.cpp
+++ Tests/UnitTests/MathTests/GPUMatrixTests.cpp
@@ -15,7 +15,6 @@ namespace Microsoft { namespace MSR { namespace CNTK { namespace Test {
 
 BOOST_AUTO_TEST_SUITE(GPUMatrixSuite)
 
-#if 0
 // TODO investigate hangs
 BOOST_FIXTURE_TEST_CASE(MatrixCopyAssignAccrossDevices, RandomSeedFixture)
 {
@@ -71,7 +70,6 @@ BOOST_FIXTURE_TEST_CASE(MatrixCopyAssignAccrossDevices, RandomSeedFixture)
         }
     }
 }
-#endif
 
 
 BOOST_FIXTURE_TEST_CASE(GPUMatrixConstructorNoFlag, RandomSeedFixture)
